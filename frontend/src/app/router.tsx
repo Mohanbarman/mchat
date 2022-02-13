33a70b3b4f49a4318817e8e713dad4eaa238b606
Mohan barman <mohanbarman9711@gmail.com>
2022-02-13 22:25:54 +0530

    Initialize frontend app

---
+++ frontend/src/app/router.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+
+export const Router = () => {
+    return (
+        <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            {/* <Route path="about" element={<About />} /> */}
+        </Routes>
+    );
+};
---
+++ frontend/src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
+import {MemoryRouter} from "react-router-dom";
+import { store } from "./redux/store";
+import { App } from "./app/app";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <ChakraProvider>
+                <MemoryRouter>
+                    <App />
+                </MemoryRouter>
+            </ChakraProvider>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
